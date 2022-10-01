c00596d96fcb019197c0fb0e066e1ba1a953014a
AJ Tech City <59399226+AJTechCity@users.noreply.github.com>
2022-10-01 18:43:04 +0100

    Fixed CONFIG DATA

--- server-side/PHP/clean/common.php
+++ server-side/PHP/clean/common.php
@@ -1,6 +1,6 @@
 <?php
     $CONFIG_DATA = array(
-        "BASE_URI"=>"https://images.example.com", //Base URI where all your iamges are located
+        "BASE_URI"=>"https://images.example.com/images", //Base URI where all your iamges are located
         "UUID_LENGTH"=>32 //Length of the UUID generated for each image
     );
 ?>
