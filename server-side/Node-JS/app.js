0956ab91b63165d03d32071e63b6c492a8e6a3a9
AJ Tech City <59399226+AJTechCity@users.noreply.github.com>
2022-10-01 18:48:40 +0100

    Begun Node.JS backend SDK

---
+++ server-side/Node-JS/app.js
@@ -0,0 +1,14 @@
+const http = reqyure('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((res, res) => {
+    res.statusCode = 200;
+    res.setHeader("Content-type", "text/plain");
+    res.end("Hello World");
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}`);
+})
