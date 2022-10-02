33f147283d15ba6b8be85c208a6a5291cae8cc04
AJ Tech City <59399226+AJTechCity@users.noreply.github.com>
2022-10-02 19:24:53 +0100

    Updated common.php to contain new configuration points

--- server-side/PHP/common.php
+++ server-side/PHP/common.php
@@ -1,6 +1,13 @@
 <?php
     $CONFIG_DATA = array(
-        "BASE_URI"=>"https://images.example.com/images", //Base URI where all your iamges are located
-        "UUID_LENGTH"=>32 //Length of the UUID generated for each image
+        "BASE_URI"=>"https://ajdp13.live/testing_images/images", //Base URI where all your iamges are located
+        "UUID_LENGTH"=>32,//Length of the UUID generated for each image
+        "FOLDER_PATH"=>"./images", //Path where images are stored (PHP Relative Path)
+        "VALID_EXTENSIONS"=>[ //Valid Image extensions stored in your directory
+            "jpg",
+            "jpeg",
+            "gif",
+            "png"
+        ]
     );
 ?>
