710aa33d973f97cf3186ea055784c51c83193311
AJ Tech City <59399226+AJTechCity@users.noreply.github.com>
2022-10-02 19:26:14 +0100

    Added json pretty print and fixed bugs

--- server-side/PHP/functions.php
+++ server-side/PHP/functions.php
@@ -7,18 +7,17 @@
         if($data!=null){
             return $data;
         }else{
-            new Returns.error(400, "Invalid JSON has been stored");
-            exit;
+            new Returns().error(400, "Invalid JSON has been stored");
         }
     }
 
     function saveJSONDatabase(array $data, string $path="php-image-database.json"){
-        $data = json_encode($data);
+        $data = json_encode($data, JSON_PRETTY_PRINT);
         if($data!=null){
             file_put_contents($path, $data);
+            return true;
         }else{
-            new Returns.error(400, "Invalid JSON data has been supplied");
-            exit;
+            new Returns().error(400, "Invalid JSON data has been supplied");
         }
     }
 
@@ -31,6 +30,21 @@
         $out[14] = "4";
         $out[19] = ["8", "9", "a", "b"][random_int(0, 3)];
         return $out;
+    }
+    
+    function relativePath($from, $to, $separator = DIRECTORY_SEPARATOR){
+        $from   = str_replace(array('/', '\\'), $separator, $from);
+        $to     = str_replace(array('/', '\\'), $separator, $to);
+    
+        $arFrom = explode($separator, rtrim($from, $separator));
+        $arTo = explode($separator, rtrim($to, $separator));
+        while(count($arFrom) && count($arTo) && ($arFrom[0] == $arTo[0]))
+        {
+            array_shift($arFrom);
+            array_shift($arTo);
+        }
+    
+        return str_pad("", count($arFrom) * 3, '..'.$separator).implode($separator, $arTo);
     }
 
 ?>
